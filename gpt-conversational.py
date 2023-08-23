98c59f544876eebfa1d5b9e74ee7ef52516094f1
mikeredev <mikeredev@gmail.com>
2023-08-23 02:21:20 +0100

    updates

--- gpt-conversational.py
+++ gpt-conversational.py
@@ -13,7 +13,6 @@ class Conversation:
         self.system_prompt = "Chat with the user."
         self.messages = [{"role": "system", "content": self.system_prompt}]
         self.counter_tokens = 0
-        self.counter_history = 0
         self.max_history = 7
 
 
@@ -24,15 +23,9 @@ class Conversation:
 
 
     # function to update history with user input
-    def update_history(self, role, text):
-        if role == "system":
-            self.messages.append({"role": "system", "content": text})
-        elif role == "user":
-            self.messages.append({"role": "user", "content": text})
-        elif role == "assistant":
-            self.messages.append({"role": "assistant", "content": text})
+    def update_payload(self, role, text):
+        self.messages.append({"role": role, "content": text})
         return self.messages
-
 
     # function to generate chat completion
     def get_bot_reply(self, messages):
@@ -40,21 +33,17 @@ class Conversation:
             model=MODEL, max_tokens=MAX_TOKENS, temperature=TEMPERATURE, messages=messages
         )
         reply = response.choices[0].message.content
-        tokens = response.usage.total_tokens
+        prompt_tokens = response.usage.prompt_tokens
+        completion_tokens = response.usage.completion_tokens
+        total_tokens = response.usage.total_tokens
         print(f"{Fore.LIGHTCYAN_EX}\n==> {reply}{Style.RESET_ALL}")
-        return {"reply": reply, "tokens": tokens}
+        return {"reply": reply, "total_tokens": total_tokens, "prompt_tokens": prompt_tokens, "completion_tokens": completion_tokens}
 
 
     # function to update token count
-    def update_token_count(self, counter_tokens, tokens):
-        counter_tokens += tokens
-        return counter_tokens
-
-
-    # function to update message history count
-    def update_message_count(self, counter_history, message_count):
-        counter_history = message_count
-        return counter_history
+    def update_token_count(self, tokens):
+        self.counter_tokens += tokens
+        return self.counter_tokens
 
 
     # function to prune history
@@ -62,17 +51,16 @@ class Conversation:
         if len_messages >= max_history:
             self.messages.pop(1)
             self.messages.pop(1)
-            return f"{len_messages}/{self.max_history}"
+        return f"{len_messages}/{self.max_history}"
 
 
     def run(self):
         while True:
-            user_input = run_function.run(self.get_user_input, "enter user input:")
-            run_function.run(self.update_history, "updating history log: user", "user", user_input)
+            user_input = run_function.run(self.get_user_input, "type your query")
+            run_function.run(self.update_payload, "updating payload [user]", "user", user_input)
             bot_reply = run_function.run(self.get_bot_reply, "rendering LLM response", self.messages)
-            run_function.run(self.update_history, "updating history log: bot", "assistant", bot_reply["reply"])
-            counter_tokens = run_function.run(self.update_token_count, "updating token counter", self.counter_tokens, bot_reply["tokens"])
-            counter_history = run_function.run(self.update_message_count, "updating message count", self.counter_history, len(self.messages))
+            run_function.run(self.update_payload, "updating payload [bot]", "assistant", bot_reply["reply"])
+            counter_tokens = run_function.run(self.update_token_count, "updating token counter", bot_reply["total_tokens"])
             run_function.run(self.prune_history, "pruning history", self.max_history, len(self.messages))
 
 
