ec7149bf6504684fd4be0f694ec9b3d34681bc76
mikeredev <mikeredev@gmail.com>
2024-01-27 04:33:05 +0000

    update

--- openai_chat.py
+++ openai_chat.py
@@ -1,10 +1,9 @@
-#!/bin/python
 """ openai_chat.py
 desc:       module for creating and returning an OpenAI chat completion
 requires:   openai
 usage:      import module openai_chat
             openai_chat.response("Reply as an expert Python developer", "How do I...")
-            openai_chat.response("Reply as an expert Python developer", "How do I...", tokens=500, model=gpt-3.5-turbo, temp=1.3)
+            openai_chat.response("Reply as an expert Python developer", "How do I...", tokens=500, model=gpt-4, temp=1.3)
             response = openai_chat.response("Reply as an expert Python developer", user_query)
             print(response["output"])
 note:       openai module expects but does not require OPENAI_API_KEY environment variable
@@ -17,6 +16,7 @@ import os
 import openai
 
 # set variables
+# the openai module expects but does not require the API key to be stored in the environment variable OPENAI_API_KEY
 model = os.environ.get("OPENAI_MODEL")
 tokens = 150
 temperature = 0
