5ca9cccd001037096100ae81b04a741eb1ae610f
mikeredev <mikeredev@gmail.com>
2024-02-06 15:25:44 +0000

    moved chat_completion to separate module

---
+++ modules/openai.py
@@ -0,0 +1,30 @@
+# import modules
+from openai import OpenAI
+
+# function to generate a chat completion using the OpenAI API
+def chat_completion(_system_prompt, _user_prompt, _tokens, _model, _temperature):
+
+    client = OpenAI()
+    _messages = [
+        {"role": "system", "content": _system_prompt},
+        {"role": "user", "content": _user_prompt},
+    ]
+
+    _response = client.chat.completions.create(
+        temperature=_temperature,
+        max_tokens=_tokens,
+        model=_model,
+        messages=_messages
+    )
+
+    _content = _response.choices[0].message.content
+    _completion_tokens = _response.usage.completion_tokens
+    _prompt_tokens = _response.usage.prompt_tokens
+    _total_tokens = _response.usage.total_tokens
+
+    return {
+        "content": _content,
+        "completion_tokens": _completion_tokens,
+        "prompt_tokens": _prompt_tokens,
+        "total_tokens": _total_tokens
+    }
