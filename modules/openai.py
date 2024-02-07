4a9036a8b7df999da070aaeded1d3054a9912067
mikeredev <mikeredev@gmail.com>
2024-02-07 02:52:38 +0000

    using Completion class

--- modules/openai.py
+++ modules/openai.py
@@ -1,30 +1,30 @@
-# import modules
 from openai import OpenAI
 
-# function to generate a chat completion using the OpenAI API
-def chat_completion(_system_prompt, _user_prompt, _max_tokens, _temperature, _model):
+class Completion:
+    def __init__(self):
+        self.client = OpenAI()
 
-    client = OpenAI()
-    _messages = [
-        {"role": "system", "content": _system_prompt},
-        {"role": "user", "content": _user_prompt},
-    ]
+    def generate(self, _system_prompt, _user_prompt, _max_tokens, _temperature, _model):
+        _messages = [
+            {"role": "system", "content": _system_prompt},
+            {"role": "user", "content": _user_prompt},
+        ]
 
-    _response = client.chat.completions.create(
-        max_tokens=_max_tokens,
-        temperature=_temperature,
-        model=_model,
-        messages=_messages
-    )
+        _response = self.client.chat.completions.create(
+            max_tokens=_max_tokens,
+            temperature=_temperature,
+            model=_model,
+            messages=_messages
+        )
 
-    _content = _response.choices[0].message.content
-    _completion_tokens = _response.usage.completion_tokens
-    _prompt_tokens = _response.usage.prompt_tokens
-    _total_tokens = _response.usage.total_tokens
+        _content = _response.choices[0].message.content
+        _prompt_tokens = _response.usage.prompt_tokens
+        _completion_tokens = _response.usage.completion_tokens
+        _total_tokens = _response.usage.total_tokens
 
-    return {
-        "content": _content,
-        "completion_tokens": _completion_tokens,
-        "prompt_tokens": _prompt_tokens,
-        "total_tokens": _total_tokens
-    }
+        return {
+            "content": _content,
+            "prompt_tokens": _prompt_tokens,
+            "completion_tokens": _completion_tokens,
+            "total_tokens": _total_tokens
+        }
