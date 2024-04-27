40a9dd0ddc7dc9feb0cd074a9abbebef67966ed4
mikeredev <mikeredev@gmail.com>
2024-04-28 00:25:29 +0100

    refactor

--- scripts/utils/rss-reader.py
+++ scripts/utils/rss-reader.py
@@ -2,7 +2,9 @@
 # uses: feedparser rofi
 
 # import standard modules
+import argparse
 from datetime import datetime
+import logging
 import os
 import subprocess
 import sys
@@ -10,84 +12,129 @@ import sys
 # import custom modules
 import feedparser
 
-# import completion module
+# import and configure llm completion
+model = os.environ.get('COHERE_CMDR_PLUS')
 sys.path.append('/home/mishi/.config/llm')
 from modules.cohere import completion
 
-# define which language model to use
-MODEL = os.environ.get('COHERE_CMDR_PLUS')
+# set model parameters
+temperature = 0.5
+max_tokens = 300
 
-# define how many entries to return from which feed
-FEED_URL = "https://feeds.bbci.co.uk/news/world/rss.xml"
-FEED_SIZE = 10
+# default feed url and items to parse (override with script args)
+feed_url = "https://feeds.bbci.co.uk/news/world/rss.xml"
+feed_items = 2
 
-# define the request parameters
-# don't set temperature too low or it will just repeat back entry titles
-TEMPERATURE = 0.5
-MAX_TOKENS = 300
-
-SYSTEM_PROMPT = f"""This is an RSS news feed. Categorise and provide a concise summary of each item using the following formatting guidelines:
+# set system prompt
+system_prompt = f"""This is an RSS news feed. Categorise and provide a concise summary of each item using these formatting guidelines:
 - Succinctly format each line in approximately ten (10) words
 - Avoid simply repeating titles
 - Ensure to include all key information, e.g., locations, main players, implications understood and highlighted
 - Categorise each item concisely, e.g., "France", "RU/UA conflict", "Hurricane"
-- Return {FEED_SIZE} lines, one for each item
-- Format the output line for each as follows: `category_name > concise_summary`
+- Return each Index entry on its own newline
+- Format each individual output line as follows: `category_name > concise_summary`
 Include no outside text or additional commentary."""
 
-# function to parse the RSS feed and return a list of entries
-def rss_reader(link, items_to_return):
-    # parse RSS feed
-    feed = feedparser.parse(link)
 
-    # store returned entries in list
-    items = []
-    for index, feed_entry in enumerate(feed.entries[:(items_to_return)], start=1):
-        item = f"Index:{index} Title:\"{feed_entry['title']}\", Content:\"{feed_entry['summary']}\""
-        items.append(item)
-    
-    # error check
-    if (len(items) != items_to_return):
-        print(f"[CRIT] {link} did not return expected entries ({items_to_return})")
+# function to setup argparse
+def set_argparse():
+    # supports defining feed url and total returned items
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", default=feed_url, \
+        help = f"the RSS feed to read (defaults {feed_url})")
+    parser.add_argument("--items", type=int, default=feed_items, \
+        help = f"how many items to return from each feed (default: {feed_items})")
+    parser.add_argument("--log-level", default="info", choices=["debug", "info", "warning", "error", "critical"], \
+        help = "set log level (default: info)")
+    return parser.parse_args()
+
+
+# function to fetch RSS items
+def get_rss_items(url, items):
+    # use feedparser to fetch the RSS feed
+    feed = feedparser.parse(url)
+    logging.info(f"Fetching {items} items from {url}")
+
+    # format each line and add to list
+    rss_items = [
+        f"Index:{index} Title:\"{entry['title']}\", Content:\"{entry['summary']}\""
+        for index, entry in enumerate(feed.entries[:items], start=1)
+    ]
+    logging.info(rss_items)
+
+    # check returned list is not empty
+    if (len(rss_items) == 0):
+        logging.error(f"No RSS feed items found at URL: {url}")
         sys.exit(1)
 
-    return items
+    # return a string of formatted RSS items
+    return str(rss_items)
 
-def main():
-    # set the user prompt to a string containing the returned RSS entries
-    USER_PROMPT = str(rss_reader(FEED_URL, FEED_SIZE))
 
-    # generate the completion
+# function to generate chat completion
+def get_response(_user_prompt, _system_prompt=system_prompt, **kwargs):
+    # load the chat completion class
     chat = completion.Completion()
+
+    # generate completion
     response = chat.generate(
-        _system_prompt=SYSTEM_PROMPT,
-        _user_prompt=USER_PROMPT,
-        _max_tokens=MAX_TOKENS,
-        _temperature=TEMPERATURE,
-        _model=MODEL)
+        _system_prompt,
+        _user_prompt,
+        _model=model,
+        _temperature=temperature,
+        _max_tokens=max_tokens
+    )
 
     # store output
     reply = response["content"]
     tokens = response["total_tokens"]
+    logging.info(f"Generated reply for {tokens} tokens")
+    return reply, tokens
 
-    # split reply string into individual lines for rofi
-    replies = reply.split('\n')
+
+# function to display output in rofi
+def show_output(rss, total_tokens, items):
+    # split reply into individual lines
+    display_items = rss.split('\n')
 
     # escape special characters and remove any blank lines
-    replies = [line.strip().replace("$", "\\$") for line in replies if line.strip()]
+    display_items = [line.strip().replace("$", "\\$") for line in display_items if line.strip()]
 
     # join the non-blank lines with newline characters
-    replies_rofi = "\n".join(replies)
+    display_list = "\n".join(display_items)
 
-    # set title of notification/response
-    title = f"Today's news for {tokens} tokens via {MODEL} | {datetime.today().date()}"
+    # set display_title of notification/response
+    display_title = f"Today's news for {total_tokens} tokens via {model} | {datetime.today().date()}"
 
     # display output in console
-    print(f"{title}:\n{replies_rofi}")
+    print(f"{display_title}:\n{display_list}")
 
     # display output using rofi
-    rofi_command = f'rofi -dmenu -mesg "{title}" <<< "{replies_rofi}" -theme "rss-reader" -theme-str "listview {{lines: {FEED_SIZE};}}"'
-    subprocess.run(rofi_command, shell=True)
+    rofi_command = f'rofi -dmenu -mesg "{display_title}" <<< "{display_list}" -theme "rss-reader" -theme-str "listview {{lines: {items};}}"'
+    subprocess.Popen(rofi_command, shell=True)
+    logging.info("Results displayed")
+
+
+# main function
+def main():
+    # setup logging and argparse
+    args = set_argparse()
+    logging.basicConfig(level=logging.getLevelName(args.log_level.upper()), format='%(levelname)s - %(message)s')
+
+    # set url/items as default unless arguments are passed
+    url = args.url
+    items = args.items
+    logging.info(f"Specified feed URL: {url}, specified items: {items}")
+
+    # fetch RSS feed items
+    rss_feed = get_rss_items(url, items)
+
+    # pass formatted string of RSS items to language model for refactoring
+    reply, total_tokens = get_response(rss_feed)
+
+    # display output with rofi
+    show_output(reply, total_tokens, items=items)
+
 
 # main entry point to script
 if __name__ == "__main__":
